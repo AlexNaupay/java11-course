c495a8ea8cef25f26857f04e4c9abf693ea2a86f
Alex Naupay <alexnaupay@gmail.com>
2023-10-09 01:19:48 -0500

    Two classes in a file, only one is public

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String... args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome! " + args[0]);
+        System.out.println("Hello and welcome! " + (args.length > 0?args[0]:""));
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -15,6 +15,27 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+
+        Animal animal = new Animal("Donkey");
+        System.out.println(animal.getName());
+
     }
 }
 
+class Animal{
+    private String name;
+
+    public Animal() {
+    }
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
