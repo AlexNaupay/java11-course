6d21453a965e03494314d768223452f84bea772a
Alex Naupay <alexnaupay@gmail.com>
2023-10-14 23:38:30 -0500

    method vs constructor

--- src/pe/devlabs/Person.java
+++ src/pe/devlabs/Person.java
@@ -1,7 +1,20 @@
 package pe.devlabs;
 
 public class Person {
+
+    public Person() {
+        System.out.println("This is a constructor");
+    }
+
+    public void Person(){
+        System.out.println("This is a method");
+    }
+
     public static void main(String... args){
         System.out.println("Hello from person");
+
+        Person person = new Person();
+        person.Person();
+
     }
 }
