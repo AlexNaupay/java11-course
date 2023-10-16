fdabfd41fa29a104d935912c8f059814a53e78e6
Alex Naupay <alexnaupay@gmail.com>
2023-10-15 23:57:29 -0500

    Fields, instance block, constructor

--- src/pe/devlabs/Person.java
+++ src/pe/devlabs/Person.java
@@ -13,8 +13,18 @@ public class Person {
     public static void main(String... args){
         System.out.println("Hello from person");
 
+        String s = "Hello String";
+
         Person person = new Person();
         person.Person();
 
+        {
+            System.out.println(s);
+        }
+
+    }
+
+    static {
+        System.out.println("Instance initializer Block is executed before constructor");
     }
 }
